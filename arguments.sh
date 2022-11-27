aab6b44f7d6ea5b7da82491fcdd56cb60d1d12f5
thomasjoseph007 <thomasvjoseph6@gmail.com>
2022-11-27 22:53:02 +0530

    arguments and parametes

---
+++ arguments.sh
@@ -0,0 +1,9 @@
+!#/bin/sh
+
+#shell scripting to learn argumets
+echo "File name: $0"
+echo "First parameter: $1"
+echo "Seconf parameter: $2"
+echo "Quoted value 1: $@"
+echo "Quoted value2 : $*"
+echo "No of parameters: $#"
